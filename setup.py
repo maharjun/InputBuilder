84c36995134a197ac10d3a4bdec68ad860905b51
Arjun Rao <arjun210493@gmail.com>
2017-11-23 17:43:14 +0100

    Shifted dependency links to github + removed flake8 dependency: version 2.1.4

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@
 from setuptools import setup
 setup(
     name="inputbuilder",
-    version="2.1.3",
+    version="2.1.4",
     packages=['ratebuilder', 'spikebuilder'],
     author="Arjun Rao",
     author_email="arjun210493@gmail.com",
     description="This module provides the infrastructure to create custom (spike/rate) builders",
     license="MIT",
     keywords="Generic Builder builder generic",
-    install_requires=['genericbuilder>=2.0.0', 'numpy', 'scipy', 'flake8'],
+    install_requires=['genericbuilder>=2.0.0', 'numpy', 'scipy'],
     provides=['ratebuilder', 'spikebuilder'],
-    dependency_links=['git+ssh://git@git.tugraz.at/GenericBuilder.git@releases#egg=genericbuilder-2.0.0']
+    dependency_links=['git+https://github.com/maharjun/GenericBuilder.git@d9e9532#egg=genericbuilder-2.0.0']
 )
